456b0c363ff25f235025303276dcd4ae455ae7f8
olebas13 <olebas13@gmail.com>
2018-12-18 18:00:27 +0200

    listing 4.2 start

--- src/main/java/com/olebas/tacocloud/tacos/security/SecurityConfig.java
+++ src/main/java/com/olebas/tacocloud/tacos/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.olebas.tacocloud.tacos.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
 }
