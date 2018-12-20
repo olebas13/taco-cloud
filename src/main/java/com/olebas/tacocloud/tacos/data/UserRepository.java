487c533e5b1634cc987ba6a47b30ddb40f066db8
olebas13 <olebas13@gmail.com>
2018-12-20 18:15:37 +0200

    listing 4.7

--- src/main/java/com/olebas/tacocloud/tacos/data/UserRepository.java
+++ src/main/java/com/olebas/tacocloud/tacos/data/UserRepository.java
@@ -1,4 +1,10 @@
 package com.olebas.tacocloud.tacos.data;
 
-public interface UserRepository {
+import com.olebas.tacocloud.tacos.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+
 }
